55ed93011f40ce58707aae33fd30cb9cc3f214a3
electricgecko <malte@wearefellows.com>
2017-06-25 20:56:27 +0200

    Break things, fix things

--- feed/index.php
+++ feed/index.php
@@ -14,7 +14,7 @@
 	// helper function to sort timeline by time of entry
 	function cmp($a, $b) { 
 		if($a->time == $b->time) { return 0 } 
-    return ($a->time > $b->time) ? -1 : 1;
+		return ($a->time > $b->time) ? -1 : 1;
 	} 
 	
 	// get followed feeds and build timeline
