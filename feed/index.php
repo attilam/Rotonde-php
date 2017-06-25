fb01dad42ead6bfec2704f571db5e6601dbd451a
electricgecko <malte@wearefellows.com>
2017-06-25 22:30:32 +0200

    Added my personal feed as a backup

--- feed/index.php
+++ feed/index.php
@@ -13,7 +13,7 @@
 	
 	// helper function to sort timeline by time of entry
 	function cmp($a, $b) { 
-		if($a->time == $b->time) { return 0 } 
+		if ($a->time == $b->time) { return 0; } 
 		return ($a->time > $b->time) ? -1 : 1;
 	} 
 	
