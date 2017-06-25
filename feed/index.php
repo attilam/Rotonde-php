f72e8b909ee20f766d57f6a217acf03004e8994a
electricgecko <malte@wearefellows.com>
2017-06-25 20:38:07 +0200

    Initial commit

---
+++ feed/index.php
@@ -0,0 +1,88 @@
+<?php
+	
+	ini_set("allow_url_fopen", 1);	  
+	$regEx = ''; // pattern to match jpg/png/gif
+	 
+	// get my feed
+	$f = '../feed.json'; // replace with your feed URL
+	$myFeed = json_decode(file_get_contents($f)); 	
+	
+	// get list of portals i'm following
+	$following = $myFeed->portal;
+	
+	// helper function to sort timeline by time of entry
+	function cmp($a, $b) { 
+    if($a->time == $b->time) {
+        return 0;
+    } 
+    return ($a->time > $b->time) ? -1 : 1;
+	} 
+	
+	// get followed feeds and build timeline
+
+	$timeline = array();	
+	
+	foreach($following as $followingUrl) {
+		$user = json_decode(file_get_contents('http://'.$followingUrl));
+		
+			foreach ($user->feed as $userPost) {
+				// attach user info to each post
+				$userPost = (object) array_merge( (array)$userPost, array( 'user' => $user->profile ) );
+				$timeline[] = $userPost;
+			}
+	}
+	
+	// sort by time posted
+	usort($timeline, 'cmp');
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="robots" content="noindex, nofollow">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+	<title>Radius &middot; Rotonde timeline</title>
+	<link rel="stylesheet" href="radius.css">
+</head>
+
+<body>
+	<header></header>
+	
+	<main>
+		<ul class="rotondeTimeline" id="rotondeTimeline">
+		<?php foreach ($timeline as $entry) :?>
+			
+			<li>
+				<main>
+					<?php if ($entry->media != '') : ?> 
+					<a target="_blank" href="<?= $entry->media ?>">
+						<img src="<?= $entry->media ?>" alt="." width="75" />
+					</a>
+					<?php endif ?>
+					
+					<p>
+						<?= $entry->text ?>
+						
+						<?php if ($entry->url != '') : ?>
+							&rarr; <a href="<?= $entry->url ?>" style="border-color: <?= $entry->user->color ?>;">Link</a>
+						<?php endif ?>	
+					</p>
+				</main>
+				<footer>
+					<img class="avatar" src="<?= $entry->user->avatar ?>" alt="Rotonde avatar" width="25" height="25"/>
+					
+					<ul class="meta">
+						<li class="user"><span class="userName"><?= $entry->user->name ?></span><span class="userLocation"> from <a href="https://www.google.de/maps/place/<?= $entry->user->location ?>" style="border-color: <?= $entry->user->color ?>;"><?= $entry->user->location ?></a></span></li>
+						<li class="time"><?= date('m.d.y - H:m:s', $entry->time) ?> (<?= $entry->time ?>)</li>
+					</ul>
+				</footer>
+			
+			</li>
+		
+		<?php endforeach ?>
+		</ul>
+	</main>	
+</body>
---
+++ post/index.php
@@ -0,0 +1,154 @@
+<?php
+		ob_start();
+		session_start();
+		
+		
+		$userinfo = array(
+    	'USER' => 'FOO'
+    );
+    
+		if(isset($_GET['logout'])) {
+	    $_SESSION['user'] = '';
+	    setcookie('rotondeSession','', time()-86400, '/');
+	    header('Location:  http://' . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
+	    exit;
+		}
+
+		if (isset($_COOKIE['rotondeSession'])) {
+		  	$_SESSION['user'] = $_COOKIE['rotondeSession'];
+		} 
+		
+		else if (isset($_POST['user'])) {
+		  	
+		    if($userinfo[$_POST['user']] == $_POST['password']) {
+		        $_SESSION['user'] = $_POST['user'];
+		        setcookie('rotondeSession',$_POST['user'], time()+86400*30, '/');
+		   
+		    } else {
+		       echo 'invalid login';
+		    }
+		}
+		ob_end_flush();
+	
+		if ($_SESSION['user']) {
+ 	   date_default_timezone_set('Europe/Berlin');
+ 	   $f = '../feed.json';
+ 	   $feed = json_decode(file_get_contents($f), true);
+ 	   
+ 			 
+ 	   if (!empty($_POST) || !empty($_GET)) {
+ 	       
+ 	       // require timestamp and text   
+ 	       if (
+ 	           isset($_POST['text'])
+ 	           && isset($_POST['timestamp'])
+ 	           && isset($_POST['submit'])   
+ 	       ) {
+ 	         
+ 	           // read post data
+ 	           
+ 	           $entry['text'] = $_POST['text'];
+ 	           $entry['time'] = $_POST['timestamp'];
+ 	           
+ 							if ($_POST['ref']) {
+ 								$entry['ref'] = $_POST['ref'];
+ 							}
+ 	
+ 							if ($_POST['media']) {
+ 								$entry['media'] = $_POST['media'];
+ 							}
+ 	
+ 							if ($_POST['url']) {
+ 								$entry['url'] = $_POST['url'];
+ 							}							
+ 							           
+ 	           // write to json
+ 	           
+ 	           array_push( $feed['feed'], $entry );
+ 	       }
+ 	       
+ 	       // update json file
+ 	       file_put_contents($f, json_encode($feed, JSON_UNESCAPED_SLASHES));
+ 	   }
+ 	  }
+?>
+
+
+
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8"/>
+	<meta name="robots" content="noindex, nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
+
+    
+	<title>Rotonde π</title>
+	<link rel="stylesheet" href="style.css">
+	
+	<script>
+        window.onload = function() {
+            var input = document.getElementById('text').focus();
+        }	
+   </script>
+</head>
+
+<body>
+	   <?php if ($_SESSION['user']): ?>
+    <header>
+        <h1>Rotonde</h1>
+        <span>π</span>
+    </header>
+
+    <form method="post" action="?">
+        
+        <textarea id="text" name="text"></textarea>
+        <label for="text">Text</label>
+        
+        <br/>
+        
+        <input id="ref" name="ref" type="text">
+        <label for="ref">Ref</label>
+        
+        <br/>
+        
+        <input id="media" name="media" type="text">
+        <label for="ref">Media</label>
+        
+        <br/>
+        
+        <input id="url" name="url" type="text">
+        <label for="ref">URL</label>
+        
+        <br/>
+        
+				<input id="timestamp" name="timestamp" type="hidden" value="<?= time() ?>">
+                      
+        <input type="submit" name="submit" value="Post">
+    </form>
+        	   	<a class="logout" id="logout" href="?logout=1">logout</a>
+	<?php else: ?>
+	    <header>
+        <h1>Rotonde</h1>
+        <span>π</span>
+    	</header>
+    	
+	    <main class="login">
+            <form name="login" action="" method="post">
+
+    	        <input type="text" name="user" value="" />
+    	        <label for="user">Username</label>
+    	        <br/>
+    	            	        
+               <input type="password" name="password" value="" />
+               <label for="password">Password</label>
+    	        
+    	        <input type="submit" name="submit" value="Submit" />
+    	   </form>	
+	    </main>
+    	
+	<?php endif; ?>
+
+</body>
